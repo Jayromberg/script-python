08e343022273ab3079f5641aeaa0c57f57067358
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2023-01-22 16:32:04 -0300

    feat: Validação da existencia do diretório

--- src/commands/font_end.py
+++ src/commands/font_end.py
@@ -1,7 +1,13 @@
 import os
 
 def front_end_commands(path, project_name):
+  directory_exist = os.path.exists(path +  project_name)
   command_mkdir = "mkdir " + path +  project_name
-  os.system(command_mkdir)
   command_react = "npx create-react-app " + path + project_name + "/web"
-  os.system(command_react)
+
+  if directory_exist:
+    os.system(command_react)
+  else:
+    os.system(command_mkdir)
+    os.system(command_react)
+    
